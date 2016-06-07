d7249ecfac5de21270199e69d4c6b5d2f7eab19f
El David Cordero Chavarría <dcorderoch215@estudiantec.cr>
2016-06-07 01:03:03 -0600

    add most Entities to SQL Scripts

--- WEBAPI/WEBAPI.Data/DB-Scripts/populate.sql
+++ WEBAPI/WEBAPI.Data/DB-Scripts/populate.sql
@@ -1,1 +1,27 @@
-INSERT INTO Product(EAN,Name,Price) VALUES (1,'Caja Leche 1L',575),(2,'Bolsa Arroz 2kg',750),(3,'Bolsa Azúcar 2kg',950);
+--Product
+INSERT INTO Product(EAN,Name,Price)
+VALUES (7441001615610,'Caja Leche 1L',575),
+       (7441003400207,'Café Volio 250g',750),
+       (7441036101225,'Bolsa Azúcar 1kg',950);
+--Customer
+INSERT INTO Customer(IDNumber, Name, LastName1, LastName2)
+VALUES ('000000000','Dinero','En','Efectivo'),
+       ('115020180','David','Cordero','Chavarría'),
+       ('115020196','Katherine','Rojas','Blanco'),
+	   ('658887623','Luis','Ramírez','Vargas');
+-- Branch Office
+INSERT INTO BranchOffice(OfficeID,Name,PhoneNumber,Location)
+VALUES (1,'Cartago','+50625558080','Oriental, Cartago'),
+       (2,'Paraíso','+50625558081','Paraíso, Cartago');
+-- Staff
+INSERT INTO Staff(BOfficeID, Name, LastName1, LastName2)
+VALUES (1, 'Rodrigo', 'Jiménez', 'Conejo'),
+       (2, 'Jimena', 'Calvo','García');
+-- Sale
+INSERT INTO Sale(IDNumber,BOfficeID,STARTOF,EOF)
+VALUES ('000000000',1,'2016-06-01 6:34:12 AM','2016-06-01 6:40:12 AM'),
+       ('115020180',2,'2016-06-01 6:34:12 AM','2016-06-01 6:40:12 AM');
+-- Staff Log
+INSERT INTO Staff_Log(StaffID, BOfficeID, Register, Start, EndOfShift, MoneyOnStart, MoneyOnExit)
+VALUES (1001, 1, 7, '2016-06-01 6:34:12 AM', '2016-06-01 6:34:12 PM',15000,235000),
+       (1002, 2, 9, '2016-06-01 6:15:00 AM', '2016-06-01 5:30:12 PM',17500,185000);
--- WEBAPI/WEBAPI.Data/DB-Scripts/restrictio.sql
+++ WEBAPI/WEBAPI.Data/DB-Scripts/restrictio.sql
@@ -1,2 +1,58 @@
+-- Product
 ALTER TABLE Product
-ADD PRIMARY KEY (EAN);
+ADD CONSTRAINT PK_Product
+PRIMARY KEY (EAN);
+--Customer
+ALTER TABLE Customer
+ADD CONSTRAINT PK_Customer
+PRIMARY KEY (IDNumber);
+-- Branch Office
+ALTER TABLE BranchOffice
+ADD CONSTRAINT PK_BranchOffice
+PRIMARY KEY (OfficeID);
+-- Staff
+ALTER TABLE Staff
+ADD CONSTRAINT PK_Staff
+PRIMARY KEY (StaffID);
+
+ALTER TABLE Staff
+ADD CONSTRAINT FK_Staff_BOffice
+FOREIGN KEY (BOfficeID)
+REFERENCES BranchOffice(OfficeID);
+-- Sale
+ALTER TABLE Sale
+ADD CONSTRAINT PK_Sale
+PRIMARY KEY (SaleID);
+
+ALTER TABLE Sale
+ADD CONSTRAINT FK_Sale_Customer
+FOREIGN KEY (IDNumber)
+REFERENCES Customer(IDNumber);
+
+ALTER TABLE Sale
+ADD CONSTRAINT FK_Sale_BranchOffice
+FOREIGN KEY(BOfficeID)
+REFERENCES BranchOffice(OfficeID);
+-- Product in Sale
+ALTER TABLE ProductInSale
+ADD CONSTRAINT PK_ProductInSale
+PRIMARY KEY (EAN, SaleID);
+
+ALTER TABLE ProductInSale
+ADD CONSTRAINT FK_ProductInSale_Sale
+FOREIGN KEY (SaleID)
+REFERENCES Sale(SaleID);
+
+ALTER TABLE ProductInSale
+ADD CONSTRAINT FK_ProductInSale_Product
+FOREIGN KEY (EAN)
+REFERENCES Product(EAN);
+-- Staff Log
+ALTER TABLE Staff_Log
+ADD CONSTRAINT PK_Staff_Log
+PRIMARY KEY (StaffID,BOfficeID,Register,Start);
+
+ALTER TABLE Staff_Log
+ADD CONSTRAINT FK_Staff_Log_BOffice
+FOREIGN KEY (BOfficeID)
+REFERENCES BranchOffice(OfficeID);
