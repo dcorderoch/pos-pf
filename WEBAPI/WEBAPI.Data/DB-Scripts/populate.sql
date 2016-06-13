c5a72652f3cac5d0150aba70b484826b011d070e
El David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-06-12 23:07:24 -0600

    update Scripts to follow administrators business rule

--- WEBAPI/WEBAPI.Data/DB-Scripts/populate.sql
+++ WEBAPI/WEBAPI.Data/DB-Scripts/populate.sql
@@ -1,25 +1,27 @@
 -- Administrators
+SET IDENTITY_INSERT Staff ON;
 INSERT INTO Staff(StaffID, BOfficeID, Name, LastName1, LastName2)
-VALUES (1, 1, 'Rodrigo', 'Jiménez', 'Conejo'), 
+VALUES (1, 1, 'David', 'Cordero', 'ChavarrÃ­a');
 --Product
 INSERT INTO Product(EAN, Name, Price, Quantity, DailyAverageSales, DaysbtwnShipment)
 VALUES (7441001615610, 'Caja Leche 1L', 575, 10, 2, 2), 
-       (7441003400207, 'Café Volio 250g', 750, 10, 2, 7), 
-       (7441036101225, 'Bolsa Azúcar 1kg', 950, 10, 2, 7);
+       (7441003400207, 'CafÃ© Volio 250g', 750, 10, 2, 7), 
+       (7441036101225, 'Bolsa AzÃºcar 1kg', 950, 10, 2, 7);
 --Customer
 INSERT INTO Customer(IDNumber, Name, LastName1, LastName2)
 VALUES ('000000000', 'Dinero', 'En', 'Efectivo'), 
-       ('115020180', 'David', 'Cordero', 'Chavarría'), 
+       ('115020180', 'David', 'Cordero', 'ChavarrÃ­a'), 
        ('115020196', 'Katherine', 'Rojas', 'Blanco'), 
-	   ('658887623', 'Luis', 'Ramírez', 'Vargas');
+	   ('658887623', 'Luis', 'RamÃ­rez', 'Vargas');
 -- Branch Office
 INSERT INTO BranchOffice(OfficeID, Name, PhoneNumber, Location)
 VALUES (1, 'Cartago', '+50625558080', 'Oriental, Cartago'), 
-       (2, 'Paraíso', '+50625558081', 'Paraíso, Cartago');
+       (2, 'ParaÃ­so', '+50625558081', 'ParaÃ­so, Cartago');
 -- Staff
+SET IDENTITY_INSERT Staff OFF;
 INSERT INTO Staff(BOfficeID, Name, LastName1, LastName2)
-VALUES (1, 'Rodrigo', 'Jiménez', 'Conejo'), 
-       (2, 'Jimena', 'Calvo', 'García');
+VALUES (1, 'Rodrigo', 'JimÃ©nez', 'Conejo'), 
+       (2, 'Jimena', 'Calvo', 'GarcÃ­a');
 -- Sale
 INSERT INTO Sale(IDNumber, StaffID, BOfficeID, STARTOF, EOF)
 VALUES ('000000000', 1001, 1, '2016-06-01 6:34:12 AM', '2016-06-01 6:40:12 AM'), 
