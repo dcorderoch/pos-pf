e0a1eba50e7f9bd07c0ceb803dd546dad2ccffcc
Manuel Zumbado <mazumbado@estudiantec.cr>
2016-06-14 01:04:38 -0600

    Add Customer Interface Service

---
+++ WEBAPI/WEBAPI.Services/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WEBAPI.Data;
+
+namespace WEBAPI.Services.Services
+{
+    public interface ICustomerService 
+    {
+        List<Customer> GetCustomers();
+        Customer GetCustomer(string pIdNumber);
+        bool SaveCustomer(Customer pNewCustomer);
+        bool UpdateCustomer(string pIdNumber, Customer pUpdatedCustomer);
+        bool DeleteCustomer(string pIdNumber);
+    }
+}
