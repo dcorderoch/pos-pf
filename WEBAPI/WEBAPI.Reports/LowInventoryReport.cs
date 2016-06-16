d6fd96c314644bb43549224e1c0bbc6c31d2bd05
Kevin <kevgiso@hotmail.com>
2016-06-15 18:16:16 -0600

    Add LowInventory Report to WEBAPI.Reports project.

---
+++ WEBAPI/WEBAPI.Reports/LowInventoryReport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WEBAPI.Reports
+{
+    public partial class LowInventoryReport : Form
+    {
+        public LowInventoryReport()
+        {
+            InitializeComponent();
+        }
+
+        private void LowInventoryReport_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'ReportDataset.Product' Puede moverla o quitarla según sea necesario.
+            this.ProductTableAdapter.Fill(this.ReportDataset.Product);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
